e74b2d8be5a464d7e621bc4be500d1f923216ed2
DanielLChang <daniellawrencechang@gmail.com>
2016-11-19 12:13:44 -0800

    Complete game

--- w2d1/lib/simon.rb
+++ w2d1/lib/simon.rb
@@ -11,7 +11,10 @@ class Simon
   end
 
   def play
-    take_turn until game_over
+    until game_over
+      take_turn
+      system("clear")
+    end
     game_over_message
     reset_game
   end
@@ -19,16 +22,30 @@ class Simon
   def take_turn
     show_sequence
     require_sequence
-    round_success_message
-    @sequence_length += 1
+    unless game_over
+      round_success_message
+      @sequence_length += 1
+    end
   end
 
   def show_sequence
     add_random_color
+    @seq.each do |color|
+      puts color
+      sleep(1)
+      system("clear")
+    end
   end
 
   def require_sequence
-
+    puts "repeat colors one at a time"
+    @seq.each do |color|
+      input = gets.chomp
+      if input != color
+        @game_over = true
+        break
+      end
+    end
   end
 
   def add_random_color
@@ -36,11 +53,11 @@ class Simon
   end
 
   def round_success_message
-
+    puts "you guessed this round correctly"
   end
 
   def game_over_message
-
+    puts "you guessed wrong. game over"
   end
 
   def reset_game
