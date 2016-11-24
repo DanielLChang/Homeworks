f9cc81edf75f77894765a786150640aa9ac542a8
DanielLChang <daniellawrencechang@gmail.com>
2016-11-23 22:57:07 -0800

    Finished big octopus and biggest fish

--- w2d4/big_octopus.rb
+++ w2d4/big_octopus.rb
@@ -1,1 +1,64 @@
+#Big O-ctopus and Biggest Fish
 fish_arr = ['fish', 'fiiish', 'fiiiiish', 'fiiiish', 'fffish', 'ffiiiiisshh', 'fsh', 'fiiiissshhhhhh']
+
+#O(n^2)
+def sluggish_oct(arr)
+
+end
+
+#O(n log n)
+class Array
+  def merge_sort(&prc)
+    prc ||= Proc.new { |num1, num2| num1 <=> num2 }
+
+    return self if self.length <= 1
+
+    mid = count / 2
+    left = self.take(mid).merge_sort(&prc)
+    right = self.drop(mid).merge_sort(&prc)
+
+    Array.merge(left, right, &prc)
+  end
+
+  private
+  def self.merge(left, right, &prc)
+    merged = []
+
+    until left.empty? || right.empty?
+      case prc.call(left.first, right.first)
+      when -1
+        merged << left.shift
+      when 0
+        merged << left.shift
+      when 1
+        merged << right.shift
+      end
+    end
+
+    merged + left + right
+  end
+end
+
+def dominant_oct(arr)
+  prc = Proc.new { |num1, num2| num1 <=> num2 }
+  arr.merge_sort(&prc)[0]
+end
+
+#O(n)
+def clever_oct(arr)
+  biggest_fish = arr.first
+  fish_arr.each do |fish|
+    biggest_fish = fish if fish.length > biggest_fish.length
+  end
+  biggest_fish
+end
+
+#Dancing Octopus
+tiles_array = ["up", "right-up", "right", "right-down", "down", "left-down", "left",  "left-up" ]
+
+#O(n)
+def slow_dance(dir, tiles_array)
+end
+
+def fast_dance(dir, new_tiles_data_structure)
+end
